7b1801207580516f76e5c0768a537b5cf2947e78
Наталья <98737558+Nkaltaeva@users.noreply.github.com>
2026-01-14 12:50:17 +0300

    Update cycle.js

--- cycle.js
+++ cycle.js
@@ -12,3 +12,14 @@ do {
     console.log(`повтор ${I + 1}`);  // сначало выполняется do и только потом идет проверка кода на верное решение. Один раз будет заходить в код, тк сначало выполняется код и только потом проверка
     I++;
 } while (I < 3) ;
+
+// обратный отсчет
+let initialCount = 10;
+function startRocketCountdown() {
+  do {
+    console.log(`запуск ${initialCount}`);
+    initialCount--;
+  } while (initialCount >= 0);
+  console.log("Старт!");
+}
+console.log(startRocketCountdown());
